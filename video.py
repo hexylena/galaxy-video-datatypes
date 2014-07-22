eea0fedf3cd40da1b12fb903cdb91f2481166bab
Eric Rasche <rasche.eric@yandex.ru>
2014-07-22 10:07:47 -0500

    initial version

---
+++ video.py
@@ -0,0 +1,26 @@
+"""
+Video classes
+"""
+
+import logging
+import magic
+
+from galaxy import eggs
+eggs.require( "bx-python" )
+
+
+import data
+
+log = logging.getLogger(__name__)
+
+# Currently these supported binary data types must be manually set on upload
+
+class Video( data.Data ):
+    pass
+
+class Mp4( Video ):
+    file_ext = "mp4"
+
+    def sniff(self, filename):
+        with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
+            return m.id_filename(filename) is 'video/mp4'
